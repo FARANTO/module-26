a37271a1e4fddcdc78eda8815b6bb4117befaa3b
ANTO <faruquerahman03@gmail.com>
2024-02-25 17:12:33 +0600

    second commit

--- rectangle.js
+++ rectangle.js
@@ -1,3 +1,18 @@
 function calculateRectangleArea(){
+    const leninput=document.getElementById('rectangle-length');
+    const lentext=leninput.value;
+    const length=parseFloat(lentext);
+    console.log(length);
     
+    // width
+    const widthinput=document.getElementById('rectangle-width');
+    const widthtext=widthinput.value;
+    const width=parseFloat(widthtext);
+    console.log(width);
+    
+    // Area
+    const recarea=length*width;
+    // display the area
+    const rectangleareaspan=document.getElementById('rectangle-area');
+    rectangleareaspan.innerText=recarea;
 }
