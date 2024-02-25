0d261ec9b0ccb293aa185f61b2bc5904117d445e
ANTO <faruquerahman03@gmail.com>
2024-02-25 17:32:01 +0600

    fourth commit

--- paralleogram.js
+++ paralleogram.js
@@ -1,6 +1,15 @@
 function calculateParaArea(){
-    const baseinput=document.getElementById('para-base');
-    const basetext=baseinput.value;
-    const base =parseFloat(basetext);
+    // const baseinput=document.getElementById('para-base');
+    // const basetext=baseinput.value;
+    // const base =parseFloat(basetext);
+    // console.log(base);
+    const base=getParaBase();
     console.log(base);
+}
+function getParaBase(){
+   
+        const baseinput=document.getElementById('para-base');
+        const basetext=baseinput.value;
+        const base =parseFloat(basetext);
+        return base;
 }
